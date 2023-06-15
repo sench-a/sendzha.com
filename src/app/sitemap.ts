3a5bfb236ce7f74f88f1067e92b14eaec08d8796
sench-a <arseni.proskura@gmail.com>
2023-06-15 17:13:53 +0300

    fix: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,13 @@
 import { siteConfig } from '@/config/site';
 
 export default async function sitemap() {
+	const home = {
+		url: siteConfig.url,
+		lastModified: new Date().toISOString().split('T')[0],
+	};
 	const routes = siteConfig.mainNav.map((nav) => ({
 		url: `${siteConfig.url}${nav.route}`,
 		lastModified: new Date().toISOString().split('T')[0],
 	}));
-	return [siteConfig.url, ...routes];
+	return [home, ...routes];
 }
