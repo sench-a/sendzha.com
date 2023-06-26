5dcc2b7f14401c0a6d8041d9899cb47b7950dcd1
sench-a <arseni.proskura@gmail.com>
2023-06-26 17:51:41 +0300

    fix: removed partytown

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-      nextScriptWorkers: true,
-  },
+  swcMinify: true,
 }
 
 module.exports = nextConfig
